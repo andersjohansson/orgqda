dd7dcf473f58b639c0747fe3a19d570fbd3c413d
Anders Johansson <mejlaandersj@gmail.com>
2022-02-20 20:20:44 +0100

    Update README with installation and coding examples
    Also reorder and rewrite some old parts

---
+++ examples/.dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode . ((orgqda-tag-files . ("file1.org" "file2.org"))
+			  (orgqda-codebook-file . "codebook.org")
+			  (eval . (orgqda-mode)))))
